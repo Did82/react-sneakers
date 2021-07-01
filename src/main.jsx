39d0b5a84e7ea9a3b201a5e98a97f94183aa31ef
Dima Kashko <kashko_di@mail.ru>
2021-07-01 19:21:34 +0300

    feat: init

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'reseter.css/css/reseter.min.css'
+import './index.css'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
