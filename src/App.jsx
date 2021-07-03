75fe7ff1df0103ef24f11c2da64c54af4bb1db47
Dima Kashko <kashko_di@mail.ru>
2021-07-04 02:22:29 +0300

    feat: add aside, part2 53.35

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div className="bg-white rounded-3xl shadow-lg max-w-wrapper mt-12 mx-auto divide-y">
-            <header className="flex justify-between p-4">
+        <div className="bg-white rounded-3xl shadow-lg max-w-wrapper mt-12 mx-auto">
+            <header className="flex justify-between p-4 border-b">
                 <div className="flex gap-3 m-4 items-center">
                     <img src="/img/logo.png" alt="logo" className="w-10 h-10"/>
                     <div>
@@ -34,6 +34,26 @@ const App = () => {
                     </ul>
                 </div>
             </header>
+
+            <aside className="absolute top-0 left-0 bg-gray-700 bg-opacity-75 h-screen w-screen z-10">
+                <div className="absolute right-0 top-0 bg-white h-screen z-20 w-96 shadow-2xl p-6">
+                    <h2 className="text-xl font-bold mb-4">Корзина</h2>
+                    <div className="flex items-center justify-between gap-4 border rounded-xl p-4">
+                        <img className="" src="/img/goods/1.jpg" alt="Sneakers" height={70} width={70}/>
+                        <div className="flex flex-col gap-2">
+                            <p className="text-sm">Мужские Кроссовки Nike Air Max 270</p>
+                            <p className="font-bold">12 999 руб.</p>
+                        </div>
+                        <button className="border-0 opacity-50 hover:opacity-100 transition-opacity">
+                            <svg className="" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white" stroke="#DBDBDB"/>
+                                <path d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z" fill="#B5B5B5"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </aside>
+
             <main className="p-4">
                 <div className="flex items-center justify-between m-4">
                     <h1 className="font-bold text-3xl">Все кроссовки</h1>
@@ -45,7 +65,7 @@ const App = () => {
                     </div>
                 </div>
                 <div className="flex justify-between p-4">
-                    <div className="border rounded-2xl w-56 p-6 flex flex-col gap-4 relative">
+                    <div className="border rounded-2xl w-56 p-6 flex flex-col gap-4 relative hover:shadow-xl transition-shadow transform hover:-translate-y-1">
                         <button className="absolute border-0">
                             <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <rect x="0.5" y="0.5" width="31" height="31" rx="6.5" fill="white" stroke="#F8F8F8"/>
@@ -135,6 +155,7 @@ const App = () => {
                     </div>
                 </div>
             </main>
+
         </div>
     )
 }
