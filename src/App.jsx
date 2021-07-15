630b23ce29a66d5f4375fe51e0b3da74583ff3c3
Dima Kashko <kashko_di@mail.ru>
2021-07-15 17:40:03 +0300

    feat: part4 full

--- src/App.jsx
+++ src/App.jsx
@@ -3,88 +3,46 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Ahead from "./components/Ahead";
 
-const store = [
-    {
-        name: 'Мужские Кроссовки Nike Blazer Mid Suede',
-        price: 12999,
-        imgUrl: '/img/goods/1.jpg'
-    },
-    {
-        name: 'Мужские Кроссовки Nike Air Max 270',
-        price: 12999,
-        imgUrl: '/img/goods/2.jpg'
-    },
-    {
-        name: 'Мужские Кроссовки Nike Blazer Mid Suede',
-        price: 8499,
-        imgUrl: '/img/goods/3.jpg'
-    },
-    {
-        name: 'Кроссовки Puma X Aka Boku Future Rider',
-        price: 8999,
-        imgUrl: '/img/goods/4.jpg'
-    },
-    {
-        name: 'Мужские Кроссовки Under Armour Curry 8',
-        price: 15199,
-        imgUrl: '/img/goods/5.jpg'
-    },
-    {
-        name: 'Мужские Кроссовки Nike Kyrie 7',
-        price: 11299,
-        imgUrl: '/img/goods/6.jpg'
-    },
-    {
-        name: 'Мужские Кроссовки Jordan Air Jordan 11',
-        price: 10799,
-        imgUrl: '/img/goods/7.jpg'
-    },
-    {
-        name: 'Мужские Кроссовки Nike LeBron XVIII',
-        price: 16499,
-        imgUrl: '/img/goods/8.jpg'
-    },
-    {
-        name: 'Мужские Кроссовки Nike Lebron XVIII Low',
-        price: 13999,
-        imgUrl: '/img/goods/9.jpg'
-    },
-    {
-        name: 'Мужские Кроссовки Nike Blazer Mid Suede',
-        price: 8499,
-        imgUrl: '/img/goods/10.jpg'
-    },
-    {
-        name: 'Кроссовки Puma X Aka Boku Future Rider',
-        price: 8999,
-        imgUrl: '/img/goods/11.jpg'
-    },
-    {
-        name: 'Мужские Кроссовки Nike Kyrie Flytrap IV',
-        price: 11299,
-        imgUrl: '/img/goods/12.jpg'
-    },
-]
-
 const App = () => {
+    const [goods, setGoods] = React.useState([]);
+    const [goodsInCart, setGoodsInCart] = React.useState([]);
+    const [isCartOpen, setIsCartOpened] = React.useState(false);
+
+    React.useEffect(() => {
+        fetch('https://60f0071af587af00179d3cf2.mockapi.io/goods')
+            .then((res) => res.json())
+            .then((json) => setGoods(json));
+    },[]);
+
+    const onAddToCart = (obj) => {
+        setGoodsInCart(prev => [...prev, obj]);
+    }
+
     return (
         <div className="bg-white rounded-3xl shadow-lg max-w-wrapper my-12 mx-auto">
-            <Header />
-            <Ahead />
+            <Header onClickCart={() => setIsCartOpened(true)}/>
+            {isCartOpen && <Ahead onClose={() => setIsCartOpened(false)} items={goodsInCart}/>}
 
             <main className="p-4">
                 <div className="flex items-center justify-between m-4">
                     <h1 className="font-bold text-3xl">Все кроссовки</h1>
                     <div className="flex border rounded-lg items-center w-60">
-                        <svg className="mx-2" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M15.25 15.25L11.8855 11.8795L15.25 15.25ZM13.75 7.375C13.75 9.06576 13.0784 10.6873 11.8828 11.8828C10.6873 13.0784 9.06576 13.75 7.375 13.75C5.68424 13.75 4.06274 13.0784 2.86719 11.8828C1.67165 10.6873 1 9.06576 1 7.375C1 5.68424 1.67165 4.06274 2.86719 2.86719C4.06274 1.67165 5.68424 1 7.375 1C9.06576 1 10.6873 1.67165 11.8828 2.86719C13.0784 4.06274 13.75 5.68424 13.75 7.375V7.375Z" stroke="#E4E4E4" stroke-width="2" stroke-linecap="round"/>
-                        </svg>
+                        <img src="/img/search.svg" alt="Search" className="mx-2"/>
                         <input className="border-0 outline-none h-10 text-gray-700" type="text" placeholder="Поиск..."/>
                     </div>
                 </div>
-                <div className="flex p-4 flex-wrap gap-8">
+                <div className="flex p-4 flex-wrap gap-8 justify-center">
                     {
-                        store.map((obj) => <Card title={obj.name} price={obj.price} img={obj.imgUrl} onClick={() => alert(obj.name)}/>)
+                        goods.map((item) => (
+                                <Card
+                                    title={item.name}
+                                    price={item.price}
+                                    imgUrl={item.imgUrl}
+                                    onClickFavorite={() => alert(item.name)}
+                                    onClickPlus={onAddToCart}
+                                />
+                            )
+                        )
                     }
                 </div>
             </main>
--- src/components/Ahead.jsx
+++ src/components/Ahead.jsx
@@ -1,91 +1,54 @@
 import React from "react";
 
-const Ahead = () => {
+const Ahead = ({ onClose, items = [] }) => {
     return (
-        <aside className="hidden absolute top-0 left-0 bg-gray-700 bg-opacity-75 h-screen w-screen z-10">
-            <div className="absolute right-0 top-0 bg-white h-screen z-20 w-96 shadow-2xl p-6 flex flex-col gap-4">
-                <div className="flex justify-between">
-                    <h2 className="text-xl font-bold mb-2">Корзина</h2>
-                    <button className="border-0 opacity-60 hover:opacity-100 transition-opacity focus:outline-none">
-                        <svg className="" width="32" height="32" viewBox="0 0 32 32" fill="none"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white"
-                                  stroke="#DBDBDB"/>
-                            <path
-                                d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z"
-                                fill="#B5B5B5"/>
-                        </svg>
+        <aside className="fixed inset-0 overflow-y-hidden z-10">
+            <div className="absolute inset-0 overflow-hidden">
+                <div onClick={onClose}
+                     className="absolute inset-0 bg-black bg-opacity-50 transition-opacity"></div>
+                <div className="absolute inset-y-0 right-0 bg-white w-96 shadow-2xl p-6 flex flex-col gap-4">
+                    <div className="flex justify-between">
+                        <h2 className="text-xl font-bold mb-2">Корзина</h2>
+                        <button onClick={onClose}
+                                className="border-0 opacity-60 hover:opacity-100 transition-opacity focus:outline-none">
+                            <img src="/img/delete.svg" alt="Close"/>
+                        </button>
+                    </div>
+                    <div className="flex flex-col gap-4 flex-grow overflow-auto">
+                        {items.map((obj) => (
+                            <div className="flex items-center justify-between gap-4 border rounded-xl p-4">
+                                <img className="" src={obj.imgUrl} alt="Sneakers" height={70} width={70}/>
+                                <div className="flex flex-col gap-2">
+                                    <p className="text-sm">{obj.title}</p>
+                                    <p className="font-bold">{obj.price} руб.</p>
+                                </div>
+                                <button
+                                    className="border-0 opacity-60 hover:opacity-100 transition-opacity focus:outline-none">
+                                    <img src="/img/delete.svg" alt="Delete" className="w-14"/>
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <div className="flex justify-between">
+                            <span>Итого:</span>
+                            <div className="border-b-2 border-dashed flex-grow mx-1.5 mb-1.5"></div>
+                            <span className="font-bold">21 498 руб.</span>
+                        </div>
+                        <div className="flex justify-between">
+                            <span>Налог 5%:</span>
+                            <div className="border-b-2 border-dashed flex-grow mx-1.5 mb-1.5"></div>
+                            <span className="font-bold">1074 руб.</span>
+                        </div>
+                    </div>
+                    <button
+                        className="flex group justify-center items-center text-center border-0 bg-btn relative h-14 text-white rounded-xl active:bg-green-700 hover:opacity-90 focus:outline-none">
+                        Оформить заказ
+                        <img
+                            className="absolute right-8 transition duration-500 ease-in-out transform group-hover:translate-x-1"
+                            src="/img/arrow.svg" alt="Arrow"/>
                     </button>
                 </div>
-                <div className="flex flex-col gap-4 flex-grow overflow-auto">
-                    <div className="flex items-center justify-between gap-4 border rounded-xl p-4">
-                        <img className="" src="/img/goods/1.jpg" alt="Sneakers" height={70} width={70}/>
-                        <div className="flex flex-col gap-2">
-                            <p className="text-sm">Мужские Кроссовки Nike Air Max 270</p>
-                            <p className="font-bold">12 999 руб.</p>
-                        </div>
-                        <button className="border-0 opacity-60 hover:opacity-100 transition-opacity focus:outline-none">
-                            <svg className="" width="32" height="32" viewBox="0 0 32 32" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white"
-                                      stroke="#DBDBDB"/>
-                                <path
-                                    d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z"
-                                    fill="#B5B5B5"/>
-                            </svg>
-                        </button>
-                    </div>
-                    <div className="flex items-center justify-between gap-4 border rounded-xl p-4">
-                        <img className="" src="/img/goods/1.jpg" alt="Sneakers" height={70} width={70}/>
-                        <div className="flex flex-col gap-2">
-                            <p className="text-sm">Мужские Кроссовки Nike Air Max 270</p>
-                            <p className="font-bold">12 999 руб.</p>
-                        </div>
-                        <button className="border-0 opacity-60 hover:opacity-100 transition-opacity focus:outline-none">
-                            <svg className="" width="32" height="32" viewBox="0 0 32 32" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white"
-                                      stroke="#DBDBDB"/>
-                                <path
-                                    d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z"
-                                    fill="#B5B5B5"/>
-                            </svg>
-                        </button>
-                    </div>
-                    <div className="flex items-center justify-between gap-4 border rounded-xl p-4">
-                        <img className="" src="/img/goods/1.jpg" alt="Sneakers" height={70} width={70}/>
-                        <div className="flex flex-col gap-2">
-                            <p className="text-sm">Мужские Кроссовки Nike Air Max 270</p>
-                            <p className="font-bold">12 999 руб.</p>
-                        </div>
-                        <button className="border-0 opacity-60 hover:opacity-100 transition-opacity focus:outline-none">
-                            <svg className="" width="32" height="32" viewBox="0 0 32 32" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <rect x="0.5" y="0.5" width="31" height="31" rx="7.5" fill="white"
-                                      stroke="#DBDBDB"/>
-                                <path
-                                    d="M20.0799 18.6155L17.6311 16.1667L20.0798 13.718C21.0241 12.7738 19.5596 11.3093 18.6154 12.2536L16.1667 14.7023L13.7179 12.2535C12.7738 11.3095 11.3095 12.7738 12.2535 13.7179L14.7023 16.1667L12.2536 18.6154C11.3093 19.5596 12.7738 21.0241 13.718 20.0798L16.1667 17.6311L18.6155 20.0799C19.5597 21.0241 21.0241 19.5597 20.0799 18.6155Z"
-                                    fill="#B5B5B5"/>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <div className="flex justify-between">
-                        <span>Итого:</span>
-                        <div className="border-b-2 border-dashed flex-grow mx-1.5 mb-1.5"> </div>
-                        <span className="font-bold">21 498 руб.</span>
-                    </div>
-                    <div className="flex justify-between">
-                        <span>Налог 5%:</span>
-                        <div className="border-b-2 border-dashed flex-grow mx-1.5 mb-1.5"> </div>
-                        <span className="font-bold">1074 руб.</span>
-                    </div>
-                </div>
-                <button className="flex group justify-center items-center text-center border-0 bg-btn relative h-14 text-white rounded-xl active:bg-green-700 hover:opacity-90 focus:outline-none">
-                    Оформить заказ
-                    <img className="absolute right-8 transition duration-500 ease-in-out transform group-hover:translate-x-1" src="/img/arrow.svg" alt="Arrow"/>
-                </button>
             </div>
         </aside>
     )
